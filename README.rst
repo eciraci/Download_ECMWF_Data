8553457ce08e435b77b00e4b36e8aae4db10ed96
Enrico Ciracì <eciraci@uci.edu>
2021-10-03 11:47:37 -0700

    10/02/2021 - fist commit: - download_bbox.py renamed to download_bbox_cds.py - download_bbox_cds.py - imports API requests in the form of JSON documents.

---
+++ README.rst
@@ -0,0 +1,42 @@
+=======================================
+Download Reanalysis Data from ECMWF
+=======================================
+|Language|
+|License|
+
+.. |Language| image:: https://img.shields.io/badge/python-v3.7-green.svg
+   :target: https://www.python.org/
+
+.. |License| image:: https://img.shields.io/badge/license-MIT-green.svg
+   :target: https://github.com/eciraci/Download_ECMWF_Data/blob/main/LICENSE
+
+A collection of scripts to download reanalysis data distributed by the
+European Centre for Medium-Range Weather Forecasts - ECMWF through:
+1. Meteorological Archival and Retrieval System (MARS) API.
+2. Climate Data Store - Copernicus API.
+
+The MARS API can be used to access old generation of ECMWF public datasets like
+ERA-Interim. A complete list of the datasets available through this
+interface is reported here:
+https://confluence.ecmwf.int/display/WEBAPI/Access+ECMWF+Public+Datasets
+
+The CDS API can be used to download all the datasets belonging to the
+ECMWF Reanalysis v5 (ERA5).
+
+
+**PYTHON DEPENDENCIES**:
+ - *ecmwfapi*: enables you to programmatically request and retrieve data
+           via HTTP from the ECMWF data archive - https://www.ecmwf.int
+ - `ecmwfapi: enables you to programmatically request and retrieve data via HTTP from the ECMWF data archive <https://www.ecmwf.int>`_
+ - `cdsapi: Climate Data Store API <https://cds.climate.copernicus.eu>`_
+ - `PyYaml: full-featured YAML framework for the Python programming language <https://pyyaml.org>`_
+ - `json: JSON encoder and decoder <https://docs.python.org/3/library/json.html>`_
+ - `numpy: The fundamental package for scientific computing with Python <https://numpy.org>`_
+
+
+License
+#######
+
+The content of this project is licensed under the
+`Creative Commons Attribution 4.0 Attribution license <https://creativecommons.org/licenses/by/4.0/>`_
+and the source code is licensed under the `MIT license <LICENSE>`_.
--- download_bbox_cds.py
+++ download_bbox_cds.py
@@ -8,6 +8,9 @@ Use the Climate Data Store API to access the ECMWF Reanalysis v5 (ERA5).
 Copernicus Data Store Web API are available here:
 https://pypi.org/project/cdsapi/
 
+How to use the API:
+https://cds.climate.copernicus.eu/api-how-to
+
 Install CDS AIP using pip: pip install cdsapi
 
 Data download requests can be sent programmatically via our API.
@@ -21,32 +24,41 @@ url: https://cds.climate.copernicus.eu/api/v2
 key: <UID>:<API key>
 verify: 0
 
-- List of the available dataset from ECMWF:
-- https://apps.ecmwf.int/datasets/
+API request general structure:
+import cdsapi
+c = cdsapi.Client()
+
+c.retrieve("dataset-short-name",
+           {... sub-selection request ...},
+           "target-file")
+
+NOTE: API request parameters are provided in the form of JSON document saved in
+     -> csd_parameters.json
 
 COMMAND LINE OPTIONS:
   -h, --help            show this help message and exit
   --path PATH, -P PATH  Absolute Output Path.
   --directory DIRECTORY, -D DIRECTORY
                         Output data directory name.
-  --f_year F_YEAR, -F F_YEAR
-                        First year of the considered period.
-  --l_year L_YEAR, -L L_YEAR
-                        Last year of the considered period.
   --boundaries BOUNDARIES, -B BOUNDARIES
                         Domain BBOX - Lon Min,Lat Max,Lat Min,Lon Max
+  --name NAME, -N NAME  Dataset Short Name
   --shapefile SHAPEFILE, -S SHAPEFILE
                         Absolute path the the shapefile containing
                         the boundaries of the Region of interest
+
 
 PYTHON DEPENDENCIES:
     argparse: Parser for command-line options, arguments and sub-commands
            https://docs.python.org/3/library/argparse.html
     numpy: The fundamental package for scientific computing with Python
            https://numpy.org/
-    PyYaml:full-featured YAML framework for the Python programming language.
-           https://pyyaml.org/
-    cdsapi: Climate Data Store API -https://cds.climate.copernicus.eu
+    json:  JSON encoder and decoder.
+           https://docs.python.org/3/library/json.html
+    cdsapi: Climate Data Store API
+           https://cds.climate.copernicus.eu
+    datetime: Basic date and time types
+           https://docs.python.org/3/library/datetime.html
 
 UPDATE HISTORY:
 """
@@ -58,7 +70,8 @@ import argparse
 import geopandas as gpd
 import numpy as np
 import cdsapi
-import yaml
+import json
+from datetime import datetime
 
 
 def create_dir(abs_path: str, dir_name: str) -> str:
@@ -89,18 +102,15 @@ def main():
     parser.add_argument('--directory', '-D', type=str, default=None,
                         help='Output data directory name.')
 
-    parser.add_argument('--f_year', '-F', type=int, default=1979,
-                        help='First year of the considered period.')
-
-    parser.add_argument('--l_year', '-L', type=int, default=2020,
-                        help='Last year of the considered period.')
-
     parser.add_argument('--boundaries', '-B', type=str, default=None,
                         help='Domain BBOX - Lon Min,Lat Max,Lat Min,Lon Max')
 
+    parser.add_argument('--name', '-N', type=str, default=None,
+                        help='Dataset Short Name')
+
     parser.add_argument('--shapefile', '-S', type=str, default=None,
                         help='Absolute path the the shapefile containing the'
-                             'boundaries of the Region of interest')
+                             ' boundaries of the Region of interest')
 
     args = parser.parse_args()
 
@@ -110,6 +120,10 @@ def main():
         sys.exit()
     else:
         data_dir = create_dir(args.path, args.directory)
+
+    if not args.name:
+        print('# - Provide dataset short name.')
+        sys.exit()
 
     if not args.boundaries and not args.shapefile:
         print('# - Provide Region of interest boundaries/bbox. See Options:')
@@ -141,66 +155,19 @@ def main():
         lon_max = float(np.max([gdf_bounds['minx'], gdf_bounds['maxx']]))
 
     # - Selected Data Domain
-    area = '{}/{}/{}/{}'.format(lat_min, lon_min, lat_max, lon_max)
+    area = [str(lat_max), str(lon_min), str(lat_min), str(lon_max)]
 
-    # - list of years of data to download
-    year_seq = range(args.f_year, args.l_year+1)
-    # - year_reference string
-    year_str = "19790101/19790201/19790301/19790401/19790501/19790601" \
-               "/19790701/19790801/19790901/19791001/19791101/19791201"
-
-    # - Python dictionary containing the selected variables as keys
-    # - and the relative ECMWF variable code.
-    var_dict = {"total_precipitation": "228.128",
-                "2_metre_temperature": "167.128",
-                "evaporation": "182.128"}
-
-    # - establish connection with ECMWF server
-    # server = ECMWFDataServer()
+    # - establish connection with Climate Data Store server
     c = cdsapi.Client()
 
-    # - Import Request Parameters from mars_parameters.yaml
-    # with open(os.path.join('.', 'parameters.yaml'), 'r') as y_fid:
-    #     req = yaml.safe_load(y_fid)
-    # req['area'] = area
+    # - Import Request Parameters from cds_parameters.json
+    with open(os.path.join('.', 'cds_parameters.json'), 'r') as j_fid:
+        req = json.loads(j_fid.read())
+    req['area'] = area
 
-    # for var in var_dict.keys():
-    #     print('# - Downloading data for:  ' + var)
-    #     for year_d in year_seq:
-    #         date_seq_str = year_str.replace("1979", str(year_d))
-    #         # -
-    #         # - update request dictionary
-    #         req["date"] = date_seq_str
-    #         req["target"] = os.path.join(data_dir,
-    #                                      var + '_' + str(year_d) + '.nc')
-    #         req["param"] = var_dict[var]
-    #         c.retrieve('reanalysis-era5-complete', req,
-    #                    var+'_{}.nc'.format(year_d))
-
-    c.retrieve(
-        'reanalysis-era5-land-monthly-means',
-        {
-            'product_type': 'monthly_averaged_reanalysis',
-            'variable': [
-                '2m_temperature', 'snowfall', 'total_precipitation',
-            ],
-            'year': [
-                '1981'
-            ],
-            'month': [
-                '01', '02', '03',
-                '04', '05', '06',
-                '07', '08', '09',
-                '10', '11', '12',
-            ],
-            'time': '00:00',
-            'area': [
-                55, 55, 5,
-                135,
-            ],
-            'format': 'netcdf',
-        },
-        os.path.join(data_dir, 'era5-request.nc'))
+    today_date = datetime.today().strftime('%Y-%m-%d')
+    c.retrieve(args.name, req,
+               os.path.join(data_dir, '{}_{}.nc'.format(args.name, today_date)))
 
 
 if __name__ == '__main__':
